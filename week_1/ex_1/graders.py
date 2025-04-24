495897e263c81c58be80e0313736624554351df6
Jayawickrama Nilusha <jayawin1@vdiubuntu175.org.aalto.fi>
2025-04-24 11:14:47 +0300

    First commit: Updated notebooks (Exercises 1-2)

---
+++ week_1/ex_1/graders.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def task1_grader(x,y,xs,ys,n):
+    mae = (1/n)*np.sum(np.sqrt((x-xs)**2 + (y-ys)**2))
+    passed = True
+    if mae > 0.05:
+        passed = False
+    return mae, passed
+
+def task1_grader_speeds(v,vs,n):
+    mae = (1/n)*np.sum(abs(vs-v))
+    passed = True
+    if mae > 0.05:
+        passed = False
+    return mae, passed
+
+
+
+
